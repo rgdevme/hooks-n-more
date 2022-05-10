94db3bc4dde58ec41549f1e1062cde5802ce70f7
Rafael Gonzalez <rafael.gonzalez81@gmail.com>
2022-05-10 02:16:19 -0400

    Imports and exports

---
+++ index.ts
@@ -0,0 +1,17 @@
+import useID from './hooks/useID'
+import useModal from './hooks/useModal'
+import useOnline from './hooks/useOnline'
+import useSearch from './hooks/useSearch'
+import useSelection from './hooks/useSelection'
+import useSorted from './hooks/useSorted'
+import useToggle from './hooks/useToggle'
+
+export {
+	useID,
+	useModal,
+	useOnline,
+	useSearch,
+	useSelection,
+	useSorted,
+	useToggle
+}
